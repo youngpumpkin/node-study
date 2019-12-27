c5e91f01fcab9cb6b59e2ae753175cc9f9f6a7f2
youngpumpkin <youngpumpkin@gmail.com>
2019-12-27 00:49:45 +0000

    web server example

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-console.log("Hello World!");
+const http = require('http');
+
+// Create an instance of the http server to handle HTTP requests
+let app = http.createServer((req, res) => {
+    // Set a response type of plain text for the response
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+
+    // Send back a response and end the connection
+    res.end('Hello World!\n');
+});
+
+// Start the server on port 3000
+app.listen(4200, '127.0.0.1');
+console.log('Node server running on port 4200');
